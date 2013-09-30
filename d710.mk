b17067cbee67cb5d26a231c39755609a3822a64a
arrrghhh <arrrghhh@xdandroid.com>
2013-09-30 00:38:08 -0600

    Remove vendorsetup
    Change-Id: I72fbf91ceca1b54c685fa2d8ad5291e2b7f01b8e

--- d710.mk
+++ d710.mk
@@ -16,6 +16,7 @@ LOCAL_PATH := device/samsung/d710
 
 # Init files
 PRODUCT_COPY_FILES := \
+    $(LOCAL_PATH)/rootdir/fstab.smdk4210:root/fstab.smdk4210 \
     $(LOCAL_PATH)/rootdir/init.smdk4210.rc:root/init.smdk4210.rc \
     $(LOCAL_PATH)/rootdir/init.smdk4210.usb.rc:root/init.smdk4210.usb.rc \
     $(LOCAL_PATH)/rootdir/ueventd.smdk4210.rc:root/ueventd.smdk4210.rc
@@ -40,9 +41,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/usr/idc/mxt224_ts_input.idc:system/usr/idc/mxt224_ts_input.idc \
     $(LOCAL_PATH)/usr/idc/sec_touchscreen.idc:system/usr/idc/sec_touchscreen.idc
 
-# Vold
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/vold.fstab:system/etc/vold.fstab
+# Fix USB transfer speeds
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vold.umsdirtyratio=20
 
 # Fix USB transfer speeds
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -55,16 +56,15 @@ PRODUCT_COPY_FILES += \
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/gps.conf:system/etc/gps.conf \
-    $(LOCAL_PATH)/configs/sirfgps.conf:system/etc/sirfgps.conf
+    $(LOCAL_PATH)/configs/sirfgps.conf:system/etc/sirfgps.conf \
+    $(LOCAL_PATH)/configs/apns-conf.xml:system/etc/apns-conf.xml
 
-# Packages
+# Sensors
 PRODUCT_PACKAGES := \
-    DeviceSettings
+    sensors.exynos4
 
 # WIMAX
 PRODUCT_PACKAGES += \
-    WiMAXSettings \
-    WiMAXHiddenMenu \
     AngryGPS \
     SprintMenu \
     SystemUpdateUI
