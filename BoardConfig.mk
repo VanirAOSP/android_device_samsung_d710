f387ccc8088ecff836231521e1d188822a6989ff
DHOMD <DHO@dho.im>
2014-02-16 12:13:37 -0500

    Test diff kernel to see if SMS can be fixed
    Change-Id: I344aee0d9828e1cede6b0ceadc1cefdab57e5ae2

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -25,7 +25,7 @@ BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/d710/bluetooth
 
 # Kernel Config
 TARGET_KERNEL_SOURCE := kernel/samsung/mokee_smdk4412
-TARGET_KERNEL_CONFIG := mokee_d710_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_d710_defconfig
 
 # Selinux
 BOARD_SEPOLICY_DIRS += \
